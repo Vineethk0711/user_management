7780906f47ee4aca0eaa8c950a2a84cd59f12863
Vineethk0711 <vineethk0711@gmail.com>
2025-05-02 19:09:15 -0400

    created keys in minio and added keys in config

--- settings/config.py
+++ settings/config.py
@@ -48,9 +48,9 @@ class Settings(BaseSettings):
         env_file_encoding = 'utf-8'
 
 # Instantiate settings to be imported in your application
-settings = Settings()
-MINIO_ENDPOINT: str = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
-MINIO_ACCESS_KEY: str = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
-MINIO_SECRET_KEY: str = os.getenv("MINIO_SECRET_KEY", "minioadmin")
-MINIO_BUCKET: str = os.getenv("MINIO_BUCKET", "profile-pictures")
-MINIO_SECURE: bool = os.getenv("MINIO_SECURE", "False").lower() == "true"
+MINIO_ENDPOINT = 'minio:9000'
+MINIO_ACCESS_KEY = 'vineeth@0711'
+MINIO_SECRET_KEY = 'vineeth@0711'
+MINIO_BUCKET = 'profile-pictures'
+MINIO_SECURE = False
+
